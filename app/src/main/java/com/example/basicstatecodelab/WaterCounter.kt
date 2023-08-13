82c45e245576bb442ce512840a15b8d458285bd7
makadu <makaduapp@outlook.com>
2023-08-13 17:21:01 -0300

    Adicionando estado no contador de copos de água

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,15 +1,45 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier){
-    val count = 0;
-    Text(text = "You've had $count glasses!",
-        modifier = Modifier.padding(16.dp))
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        
+        var count by remember { mutableStateOf(0) }
+        
+        Text(text = "You've had $count glasses!",
+            modifier = Modifier.padding(16.dp))
+        ElevatedButton(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one glass!")
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+fun WaterCounterPreview(){
+    WaterCounter()
 }
