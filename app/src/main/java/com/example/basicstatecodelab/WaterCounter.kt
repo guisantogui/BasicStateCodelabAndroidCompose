582bfcc6d681afb432c8d5e6f8b28a813043effb
makadu <makaduapp@outlook.com>
2023-08-14 08:09:00 -0300

    retornando ao código inicial para seguir no tutorial

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,74 +1,31 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
+    Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false }
-                )
-            }
-
-            Text(
-                text = "You've had $count glasses!",
-                modifier = Modifier.padding(16.dp)
-            )
-        } else {
-            Text(
-                text = "Start drinking water now!",
-                modifier = Modifier.padding(16.dp)
-            )
+            Text("You've had $count glasses.")
         }
-
-        Row(
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            ElevatedButton(
-                onClick = { count++ },
-                modifier = Modifier.padding(),
-                enabled = count < 10
-            ) {
-                Text(text = "Add one glass!")
-            }
-
-            ElevatedButton(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp),
-                enabled = count > 0
-            ) {
-                Text(text = "Clear count")
-            }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
-
-
     }
-
 }
 
 
